33b37f7fed2c12a58f68bc61326d8a07d3a39059
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-09-06 23:24:26 +0530

    Add files via upload

---
+++ problem1.c
@@ -0,0 +1,12 @@
+// Q1: Write a program to input two numbers and display their sum. // 
+
+#include <stdio.h>
+int main () {
+int a,b; 
+printf("enter a:");
+scanf("%d",&a);
+printf("enter b:");
+scanf("%d",&b);
+int sum = a + b ;
+printf("sum is : %d\n",sum);
+return 0; }
