c6f1134360726472377ba76226545126bcd11e7c
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-09-07 22:44:13 +0530

    Add files via upload

---
+++ problem11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+ int main (){int a ;
+printf("enter a number\n");
+scanf("%d", &a);
+{if (a % 2 == 0 ) 
+printf("the number is even\n");
+else( a % 2 != 0 );
+printf("the number is odd\n");}
+return 0;}
