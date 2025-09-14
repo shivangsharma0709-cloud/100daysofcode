9897f1f32d7e53233ffc9c3d3bf5a13a74cf9c8c
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-09-14 23:09:05 +0530

    Add files via upload

---
+++ problem30.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num, reverse =0;
+    printf("Enter integer: ");
+    scanf("%d", &num);
+    while (num != 0) {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num /= 10;
+    }
+    printf("Reversed number = %d\n", reverse);
+    return 0;
+}
