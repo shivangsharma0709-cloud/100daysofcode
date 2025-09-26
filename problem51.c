66b8e924483ecfa6f512d0af4a268b0f87d78f8c
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-09-26 23:43:34 +0530

    Add files via upload

---
+++ problem51.c
@@ -0,0 +1,29 @@
+/*
+Q51: Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of rows");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<n-i-1;j++)
+        {
+            printf(" ");
+        }
+        for(int j=n-i;j<=n;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
