ca557ae5034bde098b78a4b19ed729875e1cff13
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-09-26 23:43:52 +0530

    Add files via upload

---
+++ problem52.c
@@ -0,0 +1,45 @@
+/*
+
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i=i+2)
+    {
+        for(int j=1;j<=i;j++)
+        {
+        printf("*\n");
+        }
+        printf("\n");
+    }
+    for(int i=3;i>=1;i=i-2)
+    {
+        for(int j=1;j<=i;j++)
+        {
+        printf("*\n");
+        }
+        printf("\n");
+    }
+     return 0;
+}
