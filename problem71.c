b4cb65f3f263b50705b57e913056a71e3d114a65
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-11-09 20:10:42 +0530

    Add files via upload

---
+++ problem71.c
@@ -0,0 +1,28 @@
+// Q71: Read and print a matrix.//
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[100][100];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
