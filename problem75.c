19fa5997e72e862df2479c87a9c59a5bbc4fe748
shivangsharma0709-cloud <shivangsharma0709@gmail.com>
2025-11-11 21:10:07 +0530

    Add files via upload

---
+++ problem75.c
@@ -0,0 +1,51 @@
+// Q75: Add two matrices. //
+
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    int a[100][100];
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    int b[100][100];
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    if (r1 != r2 || c1 != c2) {
+        printf("Matrix addition not possible.\n");
+        return 0;
+    }
+
+    int sum[100][100];
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
